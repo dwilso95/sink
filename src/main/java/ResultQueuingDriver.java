e612140f4ba7c2dd6486aa19eac92cb1eda911e5
David <dwilso95@jhu.edu>
2019-02-01 19:07:38 -0500

    Notes and batch reading from queue

--- src/main/java/ResultQueuingDriver.java
+++ src/main/java/ResultQueuingDriver.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
@@ -8,6 +10,18 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 
+/*
+  Each call to create and next are executed in their own thread.
+  
+  Create should build a scanner, add results to a bounded LinkedBlockingQueue. (Perhaps wait when offer fails?) 
+  The queue is then kept track of in a ConcurrentHashMap  (or a more complex object which includes a "future" (boolean) 
+  of whether the scanner is done or not)?
+
+  Next, gets the Queue and "future" reads results until batch size is met or "future" is complete
+  
+
+
+*/
 public class ResultQueuingDriver {
 
   public static void main(String[] args) {
@@ -21,29 +35,23 @@ public class ResultQueuingDriver {
       while (iter.hasNext()) {
         if (results.size() < queueSize) {
           System.out.println("added " + results.offer(iter.next()));
-        } else {
-          System.out.println("Queue full. Waiting...");
-          try {
-            Thread.sleep(100);
-          } catch (InterruptedException e) {
-            System.out.println("WTF!");
-          }
         }
       }
-      System.out.println("FINISHED!");
       return 0;
     };
     final Future<Integer> queryFuture = executor.submit(query);
     final QueryResult queryResult = new QueryResult("abc", results, queryFuture);
 
     resultSetCache.put(queryResult.getQueryId(), queryResult);
-
+    int batchSize = 2;
     final Runnable poll = () -> {
+      final List<Integer> list = new ArrayList<>(batchSize);
       while (!queryResult.getResultQueue().isEmpty() || !queryResult.getQueryTaskFuture().isDone()) {
-        try {
-          System.out.println("Poll: " + queryResult.getResultQueue().take());
-        } catch (InterruptedException e) {
+        while (list.size() < batchSize) {
+          queryResult.getResultQueue().drainTo(list, batchSize - list.size());
         }
+        System.out.println("Poll: " + list);
+        list.clear();
       }
 
     };
